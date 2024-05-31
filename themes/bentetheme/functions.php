4b04e17a2b0f494e7eda66fe715d45dc45b8cf77
BenteKavsek <35726@ma-web.nl>
2024-05-31 10:25:14 +0200

    css ingesteld

--- themes/bentetheme/functions.php
+++ themes/bentetheme/functions.php
@@ -8,8 +8,3 @@ function m8prog_enqueue_styles() {
 }
 add_action( 'wp_enqueue_scripts', 'bentetheme_enqueue_styles' );
 add_theme_support( 'post-thumbnails' );
-
-echo '<pre style="background:#ed55cf; padding: 2rem; width:80%;">';
-print_r( 'Fancy mooie pagina yay :)' );
-echo '</pre>';
-die(__FILE__.':'.__LINE__);
---
+++ themes/bentetheme/index.php
@@ -0,0 +1,6 @@
+<?php
+
+echo '<pre style="background:#ed55cf; padding: 2rem; width:80%;">';
+print_r( 'Fancy mooie pagina yay :)' );
+echo '</pre>';
+die(__FILE__.':'.__LINE__);
