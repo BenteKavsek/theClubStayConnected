c484125e5d89bcc9a36a5c78c1a340636bc08524
BenteKavsek <35726@ma-web.nl>
2024-05-31 14:26:03 +0200

    wordpress loopt in de index

--- themes/bentetheme/index.php
+++ themes/bentetheme/index.php
@@ -2,9 +2,29 @@
 
 get_header();
 
-print_r( 'Main body shit :)' );
+print_r( the_title() );
+
+if ( have_posts() ) :
+
+	// Loop trough the posts
+	while ( have_posts() ) :
+	    // Load the data post data
+		the_post();
+		?>
+        <main class="container my-5">
+            <h1 class="mt-5"><?php the_title() ?></h1>
+            <div class="col-lg-8 px-0">
+                <?php the_excerpt() ?>
+            </div>
+        </main>
+	    <?php
+	endwhile;
+
+else: 
+    echo 'BITCH DIT BESTAAT NIET, U STUPID';
+endif;
 
 get_footer();
-die(__FILE__.':'.__LINE__);
+// die(__FILE__.':'.__LINE__);
 
 
