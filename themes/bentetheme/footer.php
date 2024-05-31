c194d54af6af67dfa6e0f8633953feaffe31db3d
BenteKavsek <35726@ma-web.nl>
2024-05-31 11:33:22 +0200

    header themplate en fucnties af

---
+++ themes/bentetheme/footer.php
@@ -0,0 +1,22 @@
+</main>
+<footer>
+	<section class="footer__contact">
+		<ul class="footer__contact__list">
+			<li class="footer__contact__item">Telefoon</li>
+			<li class="footer__contact__item">Mail</li>
+			<li class="footer__contact__item">Correspndatie adres</li>
+			<li class="footer__contact__item">.....</li>
+		</ul>
+	</section>
+	<section class="footer__over">
+		<h3>Over The CLub</h3>
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+			Voluptatibus, ipsum eos omnis sit amet nostrum quam ipsa tenetur quos eius numquam, 
+			consequatur quas, vitae reprehenderit blanditiis labore ut! Aut, inventore?</p>
+	</section>
+
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ themes/bentetheme/header.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+<?php wp_body_open(); ?>
+
+
+<figure class="header__figure">
+	<img class="header__logo" src="PLAATJEEE.png" alt="">
+</figure>
+
+<nav class="header__nav">
+	<ul class="header__nav__list">
+		<li class="heacer__nav__item">Home</li>
+		<li class="heacer__nav__item">Over</li>
+		<li class="heacer__nav__item">Foto's</li>
+		<li class="heacer__nav__item">Events</li>
+        <li class="heacer__nav__item">Contact</li>
+    </ul>
+</nav>
+<main>
