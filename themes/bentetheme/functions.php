91298ffe2c45e99549e63cdfd0a1019ccef66bff
BenteKavsek <35726@ma-web.nl>
2024-06-07 10:06:33 +0200

    menu's gemaakt

--- themes/bentetheme/functions.php
+++ themes/bentetheme/functions.php
@@ -8,3 +8,37 @@ function bentetheme_enqueue_styles() {
 }
 add_action( 'wp_enqueue_scripts', 'bentetheme_enqueue_styles' );
 add_theme_support( 'post-thumbnails' );
+
+
+function register_my_menus() {
+	register_nav_menus(
+		[
+			'header' => __( 'Header Menu', 'Bente-thema' ),
+			'footer-menu'  => __( 'Footer Menu', 'Bente-thema' ),
+		]
+	);
+}
+
+add_action( 'init', 'register_my_menus' );
+
+
+function add_additional_class_on_li($classes, $item, $args) {
+	if(isset($args->add_li_class)) {
+		$classes[] = $args->add_li_class;
+	}
+	return $classes;
+}
+add_filter('nav_menu_css_class', 'add_additional_class_on_li', 1, 3);
+
+
+function add_additional_class_on_a($attributes, $item, $args) {
+    if ( empty( $attributes['class'] ) ) {
+		$attributes['class'] = '';
+	}
+	
+	if(isset($args->add_a_class)) {
+		$attributes['class'] .= ' ' . $args->add_a_class;
+	}
+	return $attributes;
+}
+add_filter('nav_menu_link_attributes', 'add_additional_class_on_a', 1, 3);
--- themes/bentetheme/header.php
+++ themes/bentetheme/header.php
@@ -1,62 +1,50 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charset="<?php bloginfo('charset'); ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
 
     <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title><?php bloginfo('title') ?></title>
-	<?php wp_head(); ?>
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 
-<?php wp_body_open(); ?>
+    <?php wp_body_open(); ?>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                       data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled">Disabled</a>
-                </li>
-            </ul>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <?php
+                wp_nav_menu(
+                    [
+                        'menu'            => 'header',
+                        'link_before'     => '',
+                        'link_after'      => '',
+                        'menu_class'      => 'navbar-nav me-auto mb-2 mb-lg-0',
+                        'container'       => 'div',
+                        'container_class' => 'collapse navbar-collapse',
+                        'container_id'    => 'navbarSupportedContent',
+                        'add_li_class'    => 'nav-item',
+                        'add_a_class'     => 'nav-link',
+                    ]
+                );
+                ?>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
 
-<!-- <figure class="header__figure">
+    <!-- <figure class="header__figure">
 	<img class="header__logo" src="PLAATJEEE.png" alt="">
 </figure>
 
